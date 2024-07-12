9ea397bbc6b357353e2c1042cfda0ea44da3f6d0
Ascorsa <167785609+Ascorsa@users.noreply.github.com>
2024-07-12 19:29:42 -0400

    Add files via upload

---
+++ brand web2.cpp
@@ -0,0 +1,72 @@
+<body>
+<div data - collapse = "medium" data - animation = "default" data - duration = "400" data - easing = "ease" data - easing2 = "ease" role = "banner" class = "navigation w-nav">
+<div class = "navigation-wrap">
+<div class = "menu">
+<nav role = "navigation" class = "navigation-items w-nav-menu">
+<a href = "/" aria - current = "page" class = "navigation-item w-nav-link w--current">About< / a>
+<a href = "/" aria - current = "page" class = "navigation-item w-nav-link w--current">Work< / a>
+<a href = "/" aria - current = "page" class = "navigation-item w-nav-link w--current">experts< / a>
+<a href = "/" aria - current = "page" class = "navigation-item w-nav-link w--current">Contact< / a>
+< / nav>
+<div class = "menu-button w-nav-button"><img src = "images/menu-icon_1menu-icon.png" width = "22" alt = "" class = "menu-icon">< / div>
+< / div>
+< / div>
+< / div>
+<div class = "section">
+<section>
+<div>                                                                                                                      Today is your day to start.< / div>
+< / section>
+<div class = "container"><img src = "images/20211026_BC_210410_Flash_021_0.jpg" loading = "lazy" width = "636" sizes = "(max-width: 767px) 94vw, 636px" alt = "" srcset = "images/20211026_BC_210410_Flash_021_0-p-500.jpg 500w, images/20211026_BC_210410_Flash_021_0-p-800.jpg 800w, images/20211026_BC_210410_Flash_021_0-p-1080.jpg 1080w, images/20211026_BC_210410_Flash_021_0.jpg 1200w" class = "image">
+<div class = "motto-wrap">
+<div class = "label cc-light">Human Interface Fitness Goals< / div>
+<div class = "heading-jumbo-small">Healthy Lifestyle, Fun workout experience, and <br>learn while being fit.<br>< / div>
+< / div>
+<div class = "divider">< / div>
+<div class = "home-content-wrap">
+<div class = "w-layout-grid about-grid">
+<div id = "w-node-_86e64837-0616-515b-4568-76c147234d34-ae75e19b">
+<div class = "home-section-wrap">
+<div class = "label cc-light">What we do< / div>
+<h2 class = "section-heading">Some workouts you can expect or try at home< / h2>
+<p class = "paragraph-light">We like to mix up our workouts with cardio and weight training.<br><br>Try out our home based cardio workout< / p>
+< / div>
+<a href = "/" aria - current = "page" class = "button w-inline-block w--current">
+<div>Learn More< / div>
+< / a>
+< / div>
+<div id = "w-node-fe299c71-dc5c-673e-eaa1-c1370bca2f99-ae75e19b" style = "padding-top:56.17021276595745%" class = "w-embed-youtubevideo"><iframe src = "https://www.youtube.com/embed/_70pfgSxWSQ?rel=0&amp;controls=1&amp;autoplay=0&amp;mute=0&amp;start=0" frameborder = "0" style = "position:absolute;left:0;top:0;width:100%;height:100%;pointer-events:auto" allow = "autoplay; encrypted-media" allowfullscreen = "" title = "Home workout #3 with OhioHealth fitness trainers">< / iframe>< / div>
+< / div>
+<div class = "w-layout-grid about-grid cc-about-2">
+<div id = "w-node-_8edfa51c-08ec-1b03-88ee-d60ad758f9e0-ae75e19b" style = "padding-top:56.17021276595745%" class = "w-embed-youtubevideo"><iframe src = "https://www.youtube.com/embed/U0bhE67HuDY?rel=0&amp;controls=1&amp;autoplay=0&amp;mute=0&amp;start=0" frameborder = "0" style = "position:absolute;left:0;top:0;width:100%;height:100%;pointer-events:auto" allow = "autoplay; encrypted-media" allowfullscreen = "" title = "15 Minute Beginner Weight Training - Easy Exercises - HASfit Beginners Workout Routine - Strength">< / iframe>< / div>
+<div id = "w-node-_86e64837-0616-515b-4568-76c147234d41-ae75e19b">
+<div class = "home-section-wrap">
+<div class = "label cc-light">Weight training< / div>
+<h2 class = "section-heading">What we do cont..< / h2>
+<p class = "paragraph-light">Try to switch up your cardio workout with a weight training one. <br>Our experts can help with identfying places that you want to train< / p>
+< / div>
+<a href = "/" aria - current = "page" class = "button w-inline-block w--current">
+<div>talk to our experts< / div>
+< / a>
+< / div>
+< / div>
+< / div>
+< / div>
+< / div>
+<div class = "section">
+<div class = "container">< / div>
+< / div><img src = "images/20211026_BC_210410_Flash_021_0.jpg" loading = "lazy" width = "539" sizes = "(max-width: 767px) 100vw, 539px" alt = "" srcset = "images/20211026_BC_210410_Flash_021_0-p-500.jpg 500w, images/20211026_BC_210410_Flash_021_0-p-800.jpg 800w, images/20211026_BC_210410_Flash_021_0-p-1080.jpg 1080w, images/20211026_BC_210410_Flash_021_0.jpg 1200w" class = "image-2">
+<div class = "section cc-cta">
+<div class = "container">
+<div class = "cta-wrap">
+<div>
+<div class = "cta-text">
+<div class = "heading-jumbo-small">get started with our plans now.<br>< / div>
+< / div>
+<a href = "/" aria - current = "page" class = "button cc-jumbo-button w-inline-block w--current">
+<div>Start Now< / div>
+< / a>
+< / div>
+< / div>
+< / div>
+< / div>
+< / body>
