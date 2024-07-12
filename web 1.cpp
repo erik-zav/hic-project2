679b1e6c461d9edde019480f5fea8f1bbfed69d8
Ascorsa <167785609+Ascorsa@users.noreply.github.com>
2024-07-12 19:27:21 -0400

    Add files via upload

---
+++ web 1.cpp
@@ -0,0 +1,39 @@
+﻿<body>
+<div data - collapse = "medium" data - animation = "default" data - duration = "400" data - easing = "ease" data - easing2 = "ease" role = "banner" class = "navigation-bar w-nav">
+<div class = "w-container">
+<a href = "/" aria - current = "page" class = "w-nav-brand w--current">
+<div class = "site-name">Human Interface Fitness< / div>
+< / a>
+<nav role = "navigation" class = "navigation-menu w-nav-menu">
+<a href = "/" aria - current = "page" class = "navigation-link w-nav-link w--current">Home< / a>
+<a href = "/about" class = "navigation-link w-nav-link">Login< / a>
+<a href = "/contact" class = "navigation-link w-nav-link">Socials< / a>
+< / nav>
+<div class = "menu-button w-nav-button">
+<div class = "w-icon-nav-menu">< / div>
+< / div>
+< / div>
+< / div>
+<div class = "content-wrapper">
+<div class = "white-wrapper"><img src = "images/20211026_BC_210410_Flash_021_0---Copy.jpg" alt = "" sizes = "(max-width: 767px) 29vw, (max-width: 991px) 33vw, 34vw" srcset = "images/20211026_BC_210410_Flash_021_0---Copy-p-500.jpg 500w, images/20211026_BC_210410_Flash_021_0---Copy-p-800.jpg 800w, images/20211026_BC_210410_Flash_021_0---Copy-p-1080.jpg 1080w, images/20211026_BC_210410_Flash_021_0---Copy.jpg 1200w" class = "circle-profile">
+<p class = "site-description">Here at Human Computer Interface we strive to be the most fit summer course of all time.With the help of our partners we hope we can share this love of fitness.< / p>
+<div class = "grey-rule">< / div>
+<div class = "grey-rule">< / div>
+< / div>
+<div class = "post-content">< / div>
+< / div>
+<ul role = "list">
+<li class = "list-item"><img src = "images/32151967_web1_L3-230313-EDH-Old-Guy-Basketball-fs.jpg" loading = "lazy" width = "193" sizes = "193px" alt = "" srcset = "images/32151967_web1_L3-230313-EDH-Old-Guy-Basketball-fs-p-500.jpg 500w, images/32151967_web1_L3-230313-EDH-Old-Guy-Basketball-fs-p-800.jpg 800w, images/32151967_web1_L3-230313-EDH-Old-Guy-Basketball-fs-p-1080.jpg 1080w, images/32151967_web1_L3-230313-EDH-Old-Guy-Basketball-fs.jpg 1365w">
+<div class = "text-block">Old YMCA Guy : <br>              With years of experience that the local Y whether talking about 1980s baseball or arm workouts, information is there to share with our senior expert.<br><br>              <br>                   <br><br>‍< / div>
+<div class = "text-block-4-copy">Socials< / div>
+<div class = "w-layout-blockcontainer container w-container"><img src = "images/Facebook-Logosu---Copy.png" loading = "lazy" width = "50" sizes = "50px" alt = "" srcset = "images/Facebook-Logosu---Copy-p-500.png 500w, images/Facebook-Logosu---Copy-p-800.png 800w, images/Facebook-Logosu---Copy.png 3840w"><img src = "images/download---Copy.png" loading = "lazy" width = "41" alt = "">< / div>
+< / li>
+<li class = "list-item-2"><img src = "images/maxresdefault.jpg" loading = "lazy" width = "297" height = "Auto" alt = "" srcset = "images/maxresdefault---Copy-p-500.jpg 500w, images/maxresdefault---Copy-p-800.jpg 800w, images/maxresdefault---Copy-p-1080.jpg 1080w, images/maxresdefault.jpg 1280w" sizes = "(max-width: 479px) 86vw, 297px" class = "image-2">
+<div class = "text-block-3">Dashcam guy : <br>             With the intensity that he brings to authorities the same will be brought to your workout<br><br><br><br><br><br>< / div>
+<div class = "text-block-4-copy">Socials<br><br>< / div><img src = "images/Facebook-Logosu---Copy.png" loading = "lazy" width = "50" sizes = "50px" alt = "" srcset = "images/Facebook-Logosu---Copy-p-500.png 500w, images/Facebook-Logosu---Copy-p-800.png 800w, images/Facebook-Logosu---Copy.png 3840w"><img src = "images/download---Copy.png" loading = "lazy" width = "41" alt = "">
+< / li>
+<li class = "list-item-3"><img src = "images/BecomePersonalTrainer-1024x683---Copy.jpg" loading = "lazy" width = "267" sizes = "(max-width: 479px) 86vw, 267px" alt = "" srcset = "images/BecomePersonalTrainer-1024x683---Copy-p-500.jpg 500w, images/BecomePersonalTrainer-1024x683---Copy-p-800.jpg 800w, images/BecomePersonalTrainer-1024x683---Copy.jpg 1024w" class = "image-3">
+<div><br>Regular Fitness trainer : <br>              Just a fitness trainer.unfortunately she is fully booked so you will have to choose with one our other experts or one of the students of our class<br><br><br><br><br>‍<br><br><br><br><br><br>‍< / div>
+< / li>
+< / ul>
+< / body>
